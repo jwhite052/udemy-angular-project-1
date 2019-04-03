5ff373a864401afae6d4db77b04f2595c1c8742d
Jashua R. White <jashuawhite@gmail.com>
2019-04-03 18:24:28 -0400

    Updates

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,27 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     
     private recipes: Recipe[] = [
-      new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/11/Pindi-chana-recipe.jpg'),
-      new Recipe('Another test recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/11/Pindi-chana-recipe.jpg')
+      new Recipe(
+        'A Tasty Schnitzel',
+        'A super tasty Shnitzel - just awesome!',
+        'https://upload.wikimedia.org/wikipedia/commons/2/22/Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG',
+        [
+          new Ingredient('Meat', 1),
+          new Ingredient('French Fries', 1)
+        ]),
+      new Recipe(
+        'Big Fat Burger',
+        'What else do you need to say?',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/RedDot_Burger.jpg/2560px-RedDot_Burger.jpg',
+        [
+          new Ingredient('Buns', 2),
+          new Ingredient('Meat', 1)
+        ])
     ];
 
     getRecipes() {
